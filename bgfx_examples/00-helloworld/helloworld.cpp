ee0f748a6cc0b6554a7174caa93e425c844d3001
hanseuljun <hanseuljun@gmail.com>
2019-12-06 15:43:34 -0800

    Print something from a javascript file using bgfx.

--- bgfx_examples/00-helloworld/helloworld.cpp
+++ bgfx_examples/00-helloworld/helloworld.cpp
@@ -11,6 +11,154 @@ LRESULT CALLBACK MessageCallback(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
     return DefWindowProc(hwnd, message, wParam, lParam);
 }
 
+// Reads a file into a v8 string.
+v8::MaybeLocal<v8::String> ReadFile(v8::Isolate* isolate, const char* name) {
+    FILE* file = fopen(name, "rb");
+    if (file == NULL) return v8::MaybeLocal<v8::String>();
+
+    fseek(file, 0, SEEK_END);
+    size_t size = ftell(file);
+    rewind(file);
+
+    char* chars = new char[size + 1];
+    chars[size] = '\0';
+    for (size_t i = 0; i < size;) {
+        i += fread(&chars[i], 1, size - i, file);
+        if (ferror(file)) {
+            fclose(file);
+            return v8::MaybeLocal<v8::String>();
+        }
+    }
+    fclose(file);
+    v8::MaybeLocal<v8::String> result = v8::String::NewFromUtf8(
+        isolate, chars, v8::NewStringType::kNormal, static_cast<int>(size));
+    delete[] chars;
+    return result;
+}
+
+// Extracts a C string from a V8 Utf8Value.
+const char* ToCString(const v8::String::Utf8Value& value) {
+    return *value ? *value : "<string conversion failed>";
+}
+
+void ReportException(v8::Isolate* isolate, v8::TryCatch* try_catch) {
+    v8::HandleScope handle_scope(isolate);
+    v8::String::Utf8Value exception(isolate, try_catch->Exception());
+    const char* exception_string = ToCString(exception);
+    v8::Local<v8::Message> message = try_catch->Message();
+    if (message.IsEmpty()) {
+        // V8 didn't provide any extra information about this error; just
+        // print the exception.
+        fprintf(stderr, "%s\n", exception_string);
+    } else {
+        // Print (filename):(line number): (message).
+        v8::String::Utf8Value filename(isolate,
+                                       message->GetScriptOrigin().ResourceName());
+        v8::Local<v8::Context> context(isolate->GetCurrentContext());
+        const char* filename_string = ToCString(filename);
+        int linenum = message->GetLineNumber(context).FromJust();
+        fprintf(stderr, "%s:%i: %s\n", filename_string, linenum, exception_string);
+        // Print line of source code.
+        v8::String::Utf8Value sourceline(
+            isolate, message->GetSourceLine(context).ToLocalChecked());
+        const char* sourceline_string = ToCString(sourceline);
+        fprintf(stderr, "%s\n", sourceline_string);
+        // Print wavy underline (GetUnderline is deprecated).
+        int start = message->GetStartColumn(context).FromJust();
+        for (int i = 0; i < start; i++) {
+            fprintf(stderr, " ");
+        }
+        int end = message->GetEndColumn(context).FromJust();
+        for (int i = start; i < end; i++) {
+            fprintf(stderr, "^");
+        }
+        fprintf(stderr, "\n");
+        v8::Local<v8::Value> stack_trace_string;
+        if (try_catch->StackTrace(context).ToLocal(&stack_trace_string) &&
+            stack_trace_string->IsString() &&
+            v8::Local<v8::String>::Cast(stack_trace_string)->Length() > 0) {
+            v8::String::Utf8Value stack_trace(isolate, stack_trace_string);
+            const char* stack_trace_string = ToCString(stack_trace);
+            fprintf(stderr, "%s\n", stack_trace_string);
+        }
+    }
+}
+
+// Executes a string within the current v8 context.
+//bool ExecuteString(v8::Isolate* isolate, v8::Local<v8::String> source,
+//                   v8::Local<v8::Value> name, bool print_result,
+//                   bool report_exceptions) {
+//    v8::HandleScope handle_scope(isolate);
+//    v8::TryCatch try_catch(isolate);
+//    v8::ScriptOrigin origin(name);
+//    v8::Local<v8::Context> context(isolate->GetCurrentContext());
+//    v8::Local<v8::Script> script;
+//    if (!v8::Script::Compile(context, source, &origin).ToLocal(&script)) {
+//        // Print errors that happened during compilation.
+//        if (report_exceptions)
+//            ReportException(isolate, &try_catch);
+//        return false;
+//    } else {
+//        v8::Local<v8::Value> result;
+//        if (!script->Run(context).ToLocal(&result)) {
+//            assert(try_catch.HasCaught());
+//            // Print errors that happened during execution.
+//            if (report_exceptions)
+//                ReportException(isolate, &try_catch);
+//            return false;
+//        } else {
+//            assert(!try_catch.HasCaught());
+//            if (print_result && !result->IsUndefined()) {
+//                // If all went well and the result wasn't undefined then print
+//                // the returned value.
+//                v8::String::Utf8Value str(isolate, result);
+//                const char* cstr = ToCString(str);
+//                printf("%s\n", cstr);
+//            }
+//            return true;
+//        }
+//    }
+//}
+
+// Executes a string within the current v8 context.
+bool ExecuteString(v8::Isolate* isolate, v8::Local<v8::String> source,
+                   bool print_result,
+                   bool report_exceptions) {
+    v8::HandleScope handle_scope(isolate);
+    v8::TryCatch try_catch(isolate);
+    v8::Local<v8::Context> context(isolate->GetCurrentContext());
+    v8::Local<v8::Script> script;
+    if (!v8::Script::Compile(context, source).ToLocal(&script)) {
+        // Print errors that happened during compilation.
+        if (report_exceptions)
+            ReportException(isolate, &try_catch);
+        return false;
+    } else {
+        v8::Local<v8::Value> result;
+        if (!script->Run(context).ToLocal(&result)) {
+            assert(try_catch.HasCaught());
+            // Print errors that happened during execution.
+            if (report_exceptions)
+                ReportException(isolate, &try_catch);
+            return false;
+        } else {
+            assert(!try_catch.HasCaught());
+            if (print_result && !result->IsUndefined()) {
+                // If all went well and the result wasn't undefined then print
+                // the returned value.
+                v8::String::Utf8Value str(isolate, result);
+                const char* cstr = ToCString(str);
+                //printf("%s\n", cstr);
+                bgfx::dbgTextPrintf(0, 6, 0x0f, "Defined Result: %s", cstr);
+            }
+            v8::String::Utf8Value str(isolate, result);
+            const char* cstr = ToCString(str);
+            bgfx::dbgTextPrintf(0, 6, 0x0f, "Undefined Result: %s", cstr);
+            return true;
+        }
+    }
+}
+
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow)
 {
     std::string v8_path = veb::find_v8_path();
@@ -22,7 +170,10 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
     v8::V8::Initialize();
 
     auto logger = veb::create_msvc_logger();
-    logger->info("Hello, {}!", "World");
+    logger->info("PROJECT_SOURCE_DIR: {}", PROJECT_SOURCE_DIR);
+
+    std::string javascript_file_path = PROJECT_SOURCE_DIR;
+    javascript_file_path += "/bgfx_examples/00-helloworld/helloworld.js";
 
     HWND hwnd = veb::init_win32_window(hInstance, nCmdShow, MessageCallback);
 
@@ -147,7 +298,27 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 
             // Convert the result to a uint32 and print it.
             uint32_t number = result->Uint32Value(context).ToChecked();
-            bgfx::dbgTextPrintf(0, 4, 0x0f, "3 + 4 = %u\n", number);
+            bgfx::dbgTextPrintf(0, 4, 0x0f, "3 + 4 = %u", number);
+        }
+        {
+            v8::Local<v8::String> source;
+            if (!ReadFile(isolate, javascript_file_path.c_str()).ToLocal(&source)) {
+                isolate->ThrowException(
+                    v8::String::NewFromUtf8(isolate, "Error loading file",
+                                            v8::NewStringType::kNormal).ToLocalChecked());
+                return 1;
+            }
+
+            v8::String::Utf8Value source_value(isolate, source);
+            const char* source_string = ToCString(source_value);
+            bgfx::dbgTextPrintf(0, 5, 0x0f, "Source: %s", source_string);
+
+            if (!ExecuteString(isolate, source, false, false)) {
+                isolate->ThrowException(
+                    v8::String::NewFromUtf8(isolate, "Error executing file",
+                                            v8::NewStringType::kNormal).ToLocalChecked());
+                return 1;
+            }
         }
 
         bgfx::frame();
