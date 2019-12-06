c5424556a1c9379cedad99d3df279ad3dbcb9b5f
hanseuljun <hanseuljun@gmail.com>
2019-12-05 18:44:14 -0800

    Make main.cpp look like a boilerplate starting point.

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,65 @@
 #ifndef UNICODE
 #define UNICODE
-#endif 
+#endif
 
 #define NOMINMAX
 #include <windows.h>
 #undef NOMINMAX
 
+#include <iostream>
 #include "bx/bx.h"
 #include "bgfx/bgfx.h"
 #include "bgfx/platform.h"
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+LRESULT CALLBACK MessageCallback(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    // Register the window class.
-    const wchar_t CLASS_NAME[] = L"Sample Window Class";
+    if (message == WM_DESTROY) {
+        PostQuitMessage(0);
+        return 0;
+    }
 
+    return DefWindowProc(hwnd, message, wParam, lParam);
+}
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ PWSTR pCmdLine, _In_ int nCmdShow)
+{
     WNDCLASS wc = { };
-
-    wc.lpfnWndProc = WindowProc;
+    wc.lpfnWndProc = MessageCallback;
     wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
+    wc.lpszClassName = L"VirtualEnvironmentBrowserWindowClass";
 
     RegisterClass(&wc);
 
     // Create the window.
-
-    HWND hwnd = CreateWindowEx(
-        0,                              // Optional window styles.
-        CLASS_NAME,                     // Window class
-        L"Learn to Program Windows",    // Window text
-        WS_OVERLAPPEDWINDOW,            // Window style
-
-        // Size and position
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
-        NULL,       // Parent window    
-        NULL,       // Menu
-        hInstance,  // Instance handle
-        NULL        // Additional application data
-    );
-
-    if (hwnd == NULL)
-    {
-        return 0;
-    }
+    HWND hwnd = CreateWindowEx(0,                              // Optional window styles.
+                               wc.lpszClassName,               // Window class
+                               L"Virtual Environment Browser", // Window text
+                               WS_OVERLAPPEDWINDOW,            // Window style 
+                               CW_USEDEFAULT,                  // Size and position
+                               CW_USEDEFAULT,
+                               CW_USEDEFAULT,
+                               CW_USEDEFAULT,
+                               NULL,                           // Parent window    
+                               NULL,                           // Menu
+                               hInstance,                      // Instance handle
+                               NULL);                          // Additional application data
 
     ShowWindow(hwnd, nCmdShow);
 
-    const uint32_t DEFAULT_WIDTH = 1280;
-    const uint32_t DEFAULT_HEIGHT = 720;
-    
     bgfx::PlatformData pd;
     bx::memSet(&pd, 0, sizeof(pd));
     pd.nwh = hwnd;
     bgfx::setPlatformData(pd);
 
-    uint32_t width = DEFAULT_WIDTH;
-    uint32_t height = DEFAULT_HEIGHT;
+    uint32_t width = 1280;
+    uint32_t height = 720;
     uint32_t debug = BGFX_DEBUG_TEXT;
     uint32_t reset = BGFX_RESET_VSYNC;
 
 
     bgfx::Init init;
     init.type = bgfx::RendererType::Direct3D11;
-    init.vendorId = BGFX_PCI_ID_INTEL;
+    init.vendorId = BGFX_PCI_ID_NONE;
     init.resolution.width = width;
     init.resolution.height = height;
     init.resolution.reset = reset;
@@ -94,29 +88,4 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
     }
 
     return 0;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    switch (uMsg)
-    {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-
-
-
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
-        EndPaint(hwnd, &ps);
-    }
-    return 0;
-
-    }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
